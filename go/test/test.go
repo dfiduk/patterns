3b7134ec97b81d92ffca8fb0c33166b1f3c2d368
Vadim Sorokin <meanhero@gmail.com>
2022-07-08 16:13:57 +0300

    test -> test

--- go/test/test.go
+++ go/test/test.go
@@ -1,314 +1,119 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
-	"log"
+	"io"
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 )
 
-func splitter(s string) (result []string) {
-
-	fmt.Printf("source string is '%s'\n", s)
-
-	var opened bool
-	var oneOf string
-	var separator rune
-	for _, char := range s {
-
-		if separator == 0 && (char == '\'' || char == ' ') || char == separator {
-
-			// if char == '\'' || char == ' ' {
-			fmt.Println("' symbol was found")
-			if !opened {
-				opened = true
-				separator = char
-				fmt.Printf("is opened, separator is '%s'\n", string(separator))
-				oneOf = ""
-				continue
-			} else {
-				fmt.Printf("is closed, target string is '%s'\n", oneOf)
-				opened = false
-				separator = 0
-				result = append(result, oneOf)
-				continue
-			}
-		}
-		if opened {
-			// fmt.Println("adding")
-			oneOf += string(char)
-		}
-
-	}
-	// fmt.Println(result)
-	return result
-}
-
-// func splitter(s string) (result []string) {
-
-// 	var opened bool
-// 	var oneOf string
-// 	for _, char := range s {
-
-// 		if char == '\'' {
-// 			fmt.Println("' symbol was found")
-// 			if !opened {
-// 				fmt.Println("is opened")
-// 				opened = true
-// 				oneOf = ""
-// 				continue
+// func copier(dst io.Writer, src io.Reader) {
+// 	var buf []byte
+// 	for {
+// 		if src != nil {
+// 			n, err := src.Read(buf)
+// 			if err != nil {
+// 				fmt.Println(err)
 // 			} else {
-// 				fmt.Printf("is closed, target string is '%s'\n", oneOf)
-// 				opened = false
-// 				result = append(result, oneOf)
-// 				continue
+// 				fmt.Printf("n: %d, buf: '%s'\n", n, buf)
 // 			}
+
 // 		}
-// 		if opened {
-// 			fmt.Println("adding")
-// 			oneOf += string(char)
+// 		_, err := io.Copy(dst, src)
+// 		if err != nil {
+// 			panic(err)
 // 		}
-
 // 	}
-// 	// fmt.Println(result)
-// 	return result
 // }
 
-func reverseEnum(s string) (result string) {
+func copier(src io.Reader) {
+	buf := new(strings.Builder)
+	// check errors
+	fmt.Println(buf.String())
+	for {
+		if src != nil {
+			n, err := io.Copy(buf, src)
+			// n, err := src.Read(buf)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Printf("n: %d, buf: '%s'\n", n, buf)
+			}
 
-	// fmt.Println("TEST")
-
-	var i int
-	for i = len(s); i > 0; i-- {
-		// fmt.Println(i)
-		fmt.Println(string(s[i-1]))
-	}
-	return
-}
-
-// func CmdCall(command string, env []string) (result string, err error) {
-
-// 	var stdout bytes.Buffer
-// 	var stderr bytes.Buffer
-
-// 	// fmt.Println(reverseEnum("12345"))
-// 	fmt.Printf("%#v\n", splitter(command))
-// 	return
-
-// 	cArray := strings.Fields(command)
-// 	cmd := cArray[0]
-// 	args := cArray[1:]
-
-// 	// cmd := "/usr/local/bin/winexe --user=user%c15po82vN //185.151.147.75:445 --uploadfile /opt/mind/hostutils/mind_discovery.exe 'c:/mind//mind_discovery.exe -d -g 9d55c8de-8d89-467c-9d15-e66fb203dacf -u https://dev.mindsw.io//api/v1/apitask/update -t 120'"
-
-// 	// cmd = "/usr/local/bin/winexe"
-// 	// args := []string{
-// 	// 	"--user=user%c15po82vN",
-// 	// 	"//185.151.147.75:445",
-// 	// 	"--uploadfile=/opt/mind/hostutils/mind_discovery.exe",
-// 	// 	"c:/mind//mind_discovery.exe -g 9d55c8de-8d89-467c-9d15-e66fb203dacf -u https://dev.mindsw.io//api/v1/apitask/update -t 120",
-// 	// 	// "\"powershell.exe Start-Sleep 5\"",
-// 	// }
-// 	fmt.Printf("VSDEBUG: arg: '%#v'\n", args)
-// 	c := exec.Command(cmd, args...)
-// 	c.Env = os.Environ()
-// 	c.Env = append(c.Env, env...)
-
-// 	c.Stdout = &stdout
-
-// 	c.Stderr = &stderr
-// 	err = c.Run()
-// 	if err != nil {
-// 		err = fmt.Errorf("err: %s; cmd: '%s'; stdout: %s; stderr: %s", err, c.String(), stdout.String(), stderr.String())
-// 		return stdout.String(), err
-// 	}
-
-// 	return stdout.String(), nil
-// }
-
-// // cmdCallInExecutor - execute some commandline command with specified additional environment variables
-// // Returns stdout and possible error (includes stderr end error code)
-// func cmdCallInExecutor(cmd string, args []string, env []string) (result string, err error) {
-
-// 	// var stdout bytes.Buffer
-// 	// var stderr bytes.Buffer
-
-// 	c := exec.Command(cmd, args...)
-// 	c.Env = os.Environ()
-// 	c.Env = append(c.Env, env...)
-
-// 	// stdout, err := c.StdoutPipe()
-// 	// if err != nil {
-// 	// 	log.Fatal(err)
-// 	// }
-// 	// defer stdout.Close()
-// 	stdin, err := c.StdinPipe()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	defer stdin.Close()
-// 	// stderr, err := c.StderrPipe()
-// 	// if err != nil {
-// 	// 	log.Fatal(err)
-// 	// }
-// 	// defer stderr.Close()
-
-// 	// c.Stdout = &stdout
-
-// 	// c.Stderr = &stderr
-
-// 	fmt.Printf("VSDEBUG: command: '%s'\n", c.String())
-
-// 	// out, _ := ioutil.ReadAll(stdout)
-// 	// stderrData, _ := ioutil.ReadAll(stdout)
-// 	// fmt.Printf("%s", b)
-
-// 	err = c.Start()
-// 	// if err != nil {
-// 	// 	return stdout.String(), err
-// 	// }
-// 	if err != nil {
-// 		// err = fmt.Errorf("1: err: %s; cmd: '%s'; stdout: %s; stderr: %s", err, c.String(), stdout.String(), stderr.String())
-// 		// return stdout.String(), err
-// 		// err = fmt.Errorf("1: err: %s; cmd: '%s'; stdout: %s; stderr: %s", err, c.String(), string(out), string(stderrData))
-// 		// return string(out), err
-// 		return "", err
-// 	}
-
-// 	// time.Sleep(time.Second)
-
-// 	err = c.Wait()
-// 	if err != nil {
-// 		// err = fmt.Errorf("2: err: %s; cmd: '%s'; stdout: %s; stderr: %s", err, c.String(), stdout.String(), stderr.String())
-// 		return "", err
-// 	}
-// 	// err = c.Run()
-// 	// if err != nil {
-// 	// 	err = fmt.Errorf("err: %s; cmd: '%s'; stdout: %s; stderr: %s", err.Error(), c.String(), stdout.String(), stderr.String())
-// 	// 	return stdout.String(), err
-// 	// }
-
-// 	// return string(out), nil
-// 	return "", err
-// }
-
-func cmdCall() {
-	cmnd := exec.Command("/usr/local/bin/winexe", "--debuglevel=2", "--user=user%c15po82vN",
-		"//185.151.147.75:445", "\\mind\\mind_discovery.exe > \\mind\\file.log")
-	//cmnd.Run() // and wait
-	stdout, err := cmnd.StdoutPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stdout.Close()
-	stdin, err := cmnd.StdinPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stdin.Close()
-	stderr, err := cmnd.StderrPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stderr.Close()
-	err = cmnd.Start()
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = cmnd.Wait()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	b, _ := ioutil.ReadAll(stdout)
-	fmt.Printf("%s", b)
-}
-
-// cmdCallInExecutor - execute some commandline command with specified additional environment variables
-// Returns stdout and possible error (includes stderr end error code)
-func cmdCallInExecutor(cmd string, args []string, env []string) (result string, err error) {
-
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
-
-	c := exec.Command(cmd, args...)
-	c.Env = os.Environ()
-	c.Env = append(c.Env, env...)
-
-	stdin, err := c.StdinPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stdin.Close()
-	c.Stdout = &stdout
-	c.Stderr = &stderr
-
-	fmt.Printf("VSDEBUG: command: '%s'\n", c.String())
-
-	err = c.Start()
-	if err != nil {
-		fmt.Println("11111")
-		if len(stderr.String()) == 0 && strings.Contains(strings.ToLower(stdout.String()), "error") {
-			stderr = stdout
 		}
-		err = fmt.Errorf("1: err: %s; cmd: '%s'; stdout: %s; stderr: %s", err, c.String(), stdout.String(), stderr.String())
-		return stdout.String(), err
+		time.Sleep(1 * time.Second)
+		// _, err := io.Copy(dst, src)
+		// if err != nil {
+		// 	panic(err)
+		// }
 	}
-
-	err = c.Wait()
-	if err != nil {
-		fmt.Println("21111")
-		if len(stderr.String()) == 0 && strings.Contains(strings.ToLower(stdout.String()), "error") {
-			stderr = stdout
-		}
-		err = fmt.Errorf("1: err: %s; cmd: '%s'; stdout: %s; stderr: %s", err, c.String(), stdout.String(), stderr.String())
-		return stdout.String(), err
-	}
-
-	if strings.Contains(strings.ToLower(stdout.String()), "error") {
-		stderr = stdout
-		err = fmt.Errorf("1: err: %s; cmd: '%s'; stdout: %s; stderr: %s", "Exit Code: 999", c.String(), stdout.String(), stderr.String())
-		return stdout.String(), err
-	}
-
-	return stdout.String(), err
 }
 
 func main() {
 
-	// cmdCall()
-	// os.Exit(0)
+	c1 := exec.Command("c:/mind/agent/agent/ncat.exe", "-l", "-p 9000")
+	c1.Env = os.Environ()
+	c2 := exec.Command("c:/mind/agent/agent/dd.exe", `of='\\.\Volume{bdd7570a-0000-0000-0000-501f00000000}\test.txt`, `bs=16M`, `--progress`)
+	c2.Env = os.Environ()
 
-	cmd := "/usr/local/bin/winexe"
-	args := []string{
-		"//185.151.147.75:445",
-		"--user=user%c15po82vN",
-		"--debuglevel=0",
-		"--interactive=0",
-		// "--ostype=1",
-		// "-N",
-		// "--reinstall",
-		// "--runas=user%c15po82vN",
-		"--uploadfile=/opt/mind/hostutils/mind_discovery.exe",
-		// "'powerhell.exe Start-Sleep 20'",
-		// "-V",
-		// "c:/mind//mind_discovery.exe -g 9d55c8de-8d89-467c-9d15-e66fb203dacf -u https://dev.mindsw.io//api/v1/apitask/update -t 120",
-		"powershell.exe Start-Process -ArgumentList '-g 00b8c137-2f05-4f39-92b6-c2cf53cba06c -u https://dev.mindsw.io//api/v1/apitask/update -t 120' -FilePath c:/mind//mind_discovery.exe",
-		// `"powershell.exe Start-Process -ArgumentList '-g d2b36e28-a641-4439-8590-7e2854dc749c -u https://dev.mindsw.io//api/v1/apitask/update -t 120' -FilePath c:/mind//mind_discovery.exe"`,
-		// "'powershell.exe'",
-	}
-
-	// "powershell.exe Start-Process -ArgumentList '-g 00b8c137-2f05-4f39-92b6-c2cf53cba06c -u https://dev.mindsw.io//api/v1/apitask/update -t 120' -FilePath c:/mind//mind_discovery.exe"
-	res, err := cmdCallInExecutor(cmd, args, []string{})
-	// var stdoutPipe io.ReadCloser
-	// CmdCallOutput(cmd, args, []string{}, &stdoutPipe)
-	// cmd := "/usr/local/bin/winexe //185.151.147.75:445 --user=user%c15po82vN '--uploadfile=/opt/mind/hostutils/mind_discovery.exe' 'c:/mind//mind_discovery.exe -g 9d55c8de-8d89-467c-9d15-e66fb203dacf -u https://dev.mindsw.io//api/v1/apitask/update -t 120'"
-	// res, err := CmdCall(cmd, []string{})
+	_, err := c1.StdinPipe()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(res)
+	r1, err := c1.StdoutPipe()
+	if err != nil {
+		panic(err)
+	}
+	stderr, err := c1.StderrPipe()
+	if err != nil {
+		fmt.Println(stderr)
+		panic(err)
+	}
+
+	// w2, err := c2.StdinPipe()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	err = c1.Start()
+	if err != nil {
+		panic(err)
+	}
+
+	// err = c2.Start()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// go copier(w2, r1)
+	go copier(r1)
+
+	// for i := 0; i < 100; i++ {
+	// 	_, err = w1.Write([]byte(fmt.Sprintf("Number: %d\r\n", i)))
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// }
+
+	// _, err = w1.Write([]byte("TEST\n"))
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// _, err = w1.Write([]byte("TEST2\n"))
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	err = c1.Wait()
+	if err != nil {
+		panic(err)
+	}
+
+	// err = c2.Wait()
+	// if err != nil {
+	// 	panic(err)
+	// }
 }
--- go/test/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-
-/usr/local/bin/winexe //185.151.147.75:445 --user=user%c15po82vN --debuglevel=5 --interactive=0 --ostype=1 --kerberos=yes 'c:\mind\mind_discovery.exe'
